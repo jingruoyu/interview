ab1aa5e754a5124c7be7ba18b9fc7ee860e00c50
jingruoyu <ruoyujing@sohu-inc.com>
2017-09-07 12:20:07 +0800

    吃饭random

---
+++ getEatLocation.js
@@ -0,0 +1,7 @@
+function getEatLation () {
+	var list = ["螺蛳粉","711","小江南","阿里","哈尔滨坛肉","人人","知味湘","桂林米粉","顺口溜","满里","麻辣烫","重庆小面","周三老面馆"]
+	var index = parseInt(list.length*Math.random());
+	console.log(index);
+	console.log(list[index]);
+}
+getEatLation();
