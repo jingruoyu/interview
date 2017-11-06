4713d6e4b8abba802a61cf76add36efa97183213
jingruoyu <ruoyujing@sohu-inc.com>
2017-11-06 10:46:48 +0800

    面试zancun

--- getEatLocation.js
+++ getEatLocation.js
@@ -1,7 +1,37 @@
 function getEatLation () {
-	var list = ["螺蛳粉","711","小江南","阿里","哈尔滨坛肉","人人","知味湘","桂林米粉","顺口溜","满里","麻辣烫","重庆小面","周三老面馆"]
+	var list = [];
+	var weight = {
+		"螺蛳粉": 0,
+		"711": 0,
+		"小江南": 0,
+		"阿里": 0,
+		"哈尔滨坛肉": 0,
+		"人人": 0,
+		"知味湘": 0,
+		"桂林米粉": 0,
+		"顺口溜": 0,
+		"满里": 0,
+		"麻辣烫": 0,
+		"重庆小面": 0,
+		"周三老面馆": 0,
+		"外卖": 0
+	}
+	for (var key in weight) {
+		if(weight[key] === 0) continue;
+		for(var i=0;i<weight[key];i++) {
+			list.push(key);
+		}
+	}
 	var index = parseInt(list.length*Math.random());
-	console.log(index);
+	var date = new Date();
+	console.log(formatDate(date));
 	console.log(list[index]);
+}
+function formatDate (date) {
+	var date = new Date(date);
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	return year + "-" + (month < 10?"0"+month:month) + "-" + (day < 10?"0"+day:day)
 }
 getEatLation();
